13e8f1eb4cdaf28bf115bdd1ae84df5769bd0d1f
sini <506945295@qq.com>
2015-12-01 10:05:58 +0800

    test 测试

---
+++ TestGitServer/TestGitServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitServer
+//
+//  Created by nctaarena on 15/12/1.
+//  Copyright © 2015年 nctaarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
